2474056bca9463f311bfbfb8e9ef768b64175412
mudigonda <himansh02@hotmail.com>
2020-10-17 14:17:48 +0530

    Update nmap_status_checker.sh

--- nmap_status_checker.sh
+++ nmap_status_checker.sh
@@ -1,8 +1,12 @@
+#! /usr/bin/bash
+
 dpkg -s nmap &> /dev/null
 if [ $? -ne 0 ]
 then
 	chmod +x nmap_installer.sh
+	echo " =>  Preparing to execute nmap_installer.sh" >> discoverr.log
 	sudo sh nmap_installer.sh
+    echo " =>  Installed nmap" >> discoverr.log 
 else
     echo " =>  Nmap is installed" >> discoverr.log
 fi

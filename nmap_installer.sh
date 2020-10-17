2ae6dacd777d6177a7cda66c367baaa7caeb927d
mudigonda <himansh02@hotmail.com>
2020-10-17 14:17:29 +0530

    Delete nmap_installer.sh

--- nmap_installer.sh
+++
@@ -1,6 +0,0 @@
-echo " =>  Nmap is not installed" >> discoverr.log
-echo " =>  Performing a system update" >> discoverr.log
-sudo apt-get update
-echo " =>  Installing nmap" >> discoverr.log
-sudo apt-get install nmap
-echo " =>  Nmap is now installed" >> discoverr.log

e8b5da96a6032f05bdf99b01016597da22a111d3
mudigonda <himansh02@hotmail.com>
2020-10-17 13:44:56 +0530

    Add files via upload
    createDir.sh creates directories required for the discoverr script

---
+++ createDir.sh
@@ -0,0 +1,12 @@
+discoverr_path=($PWD)
+# create dir for the tool
+mkdir discoverr/
+
+# navigate to the folder
+cd discoverr/
+
+# create dir to store outputs
+cd logs/
+
+# touch a new file to store data
+sudo touch discoverr.log

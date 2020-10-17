a067538a81e6cdf39f267f7d8415563eb4d51acd
mudigonda <himansh02@hotmail.com>
2020-10-17 13:59:30 +0530

    Add files via upload

---
+++ timestamper.sh
@@ -0,0 +1,9 @@
+# appending timestamp to log
+echo " " >> discoverr.log
+echo "############################################" >> discoverr.log;
+echo "`date -u`" >> discoverr.log
+echo "  Script name : discoverr.sh"
+echo "  Script by   : hhimmmmii"
+echo "############################################" >> discoverr.log;
+echo " " >> discoverr.log
+echo " " >> discoverr.log
